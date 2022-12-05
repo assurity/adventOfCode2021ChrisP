5b8898407ea930bba0f67cee10174ad86f56a84f
ChrisP <chris.pollard@assurity.co.nz>
2022-12-05 20:50:22 +1300

    working.. could do better on fn defn

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,47 @@
 use std::fs::File;
 // use std::fs::OpenOptions;
-use std::io::{BufReader, BufRead, Error};
+use std::io::{BufReader, BufRead, Lines};
+use std::iter::Map;
+use std::path::Path;
 
-fn main() -> Result<(), Error> {
+fn lines_from_file(filename: impl AsRef<Path>) -> Map<Lines<BufReader<File>>, fn(std::io::Result<String>) -> String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+}
+
+fn main() {
     let path = "input.txt";
 
-    // let mut output = File::create(path)?;
-    // let mut output = OpenOptions.``
-    // write!(output, "Rust\n💖\nFun")?;
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
     let mut previous = -1;
-    let mut current;
     let mut increase_count = 0;
-    for line in buffered.lines() {
-        // println!("{}", line?);
-        let my_int = line?.parse::<i32>().unwrap();
-        // println!("{}", my_int);
-        current = my_int;
-        if previous != -1 {
-            if current > previous {
-                print_evaluation(&previous, &current, "increased");
-                increase_count = increase_count + 1;
-            } else {
-                print_evaluation(&previous, &current, "decreased");
-            }
-        } else {
+
+    let lines = lines_from_file(&path);
+    let numbers = lines.map(|l| l.parse::<i32>().unwrap());
+
+    let _result: Vec<i32> = numbers.map(|current| {
+        if first_record(previous) {
             println!("skipping first");
+            previous = current;
+            return increase_count;
+        }
+
+        if current > previous {
+            print_evaluation(&previous, &current, "increased");
+            increase_count = increase_count + 1;
+        } else {
+            print_evaluation(&previous, &current, "decreased");
         }
         previous = current;
-
-    }
+        return increase_count;
+    }).collect();
 
     println!("Total increase count = {}", increase_count);
-    Ok(())
+}
+
+fn first_record(previous: i32) -> bool {
+    previous == -1
 }
 
 fn print_evaluation(previous: &i32, current: &i32, direction_label: &str) {
